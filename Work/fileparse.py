1c2e51b9ccd13c1c401cdb26428e0d3cc240c50e
fractal-mind <29667605+fractal-mind@users.noreply.github.com>
2023-10-07 10:51:19 -0700

    Added fuctionality to work with CSV files without headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,7 +6,8 @@ import csv
 from typing import Optional
 from icecream import ic
 
-def parse_csv(filename: str, select: Optional[list]= None, types: Optional[list]= None) -> list:
+def parse_csv(filename: str, has_headers: True, select: Optional[list]= None,
+              types: Optional[list]= None) -> list:
     '''
     Parse a CSV file into a list of records
     '''
@@ -14,11 +15,11 @@ def parse_csv(filename: str, select: Optional[list]= None, types: Optional[list]
         rows = csv.reader(f)
 
         # Read file headers
-        headers = next(rows)
-        ic(headers)
-        total_headers = headers
-        total_header_count = len(total_headers)
-
+        if has_headers == False:
+            pass
+        else:
+            headers = next(rows)
+        
         # If a column selector is provided, find indices of specified columns
         # and filter selection for headers used for resulting dictionaries
         if select:
@@ -26,28 +27,36 @@ def parse_csv(filename: str, select: Optional[list]= None, types: Optional[list]
             headers = select
         else:
             indices = []
-        
-        if types:
-            type_indices = [types.index(type) for type in types]
-            ic(type_indices)
-        else:
-            type_indices = []
 
         records = []
         for row in rows:
             if not row:     # Skip rows with no data
                 continue
+            
             # Filter the row if specific columns were selected
             if indices:
                 row = [ row[index] for index in indices]
             
             # If available, map the types to values
-            if types:
+            if types == None:
+                pass
+            elif types:
                 row = [func(val) for func, val in zip(types, row)]
+                
                 # Make a dictionary
-                record = dict(zip(headers, row))
-                records.append(record)
+                #If there are no headers, create list of tuples instead of dict
+                if has_headers == False:
+                    row = (row[0], row[1])
+                    ic(row)
+                    ic(has_headers)
+                    #record = zip(row[0], row[1])
+                    records.append(row)
+                else:
+                    record = dict(zip(headers, row))
+                    ic(row)
+                    ic(has_headers)
+                    records.append(record)
             else:
                 print("ERROR: Too many or too few types listed")
-                    
+        
         return records
